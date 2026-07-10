d71e9ce02f0236fe81cee6508277b7906b994aef
geraldwang-spec <65798968+geraldwang-spec@users.noreply.github.com>
2026-07-10 22:04:07 +0800

    modify script and config for auto setup server

--- deplay.sh
+++ deplay.sh
@@ -3,23 +3,28 @@ set -e
 
 echo "[1/5] checking and craeting folder for local development and database ..."
 mkdir -p ./sqldata
-mkdir -p ./coreVcl
+mkdir -p ./coreVcl/templates ./coreVcl/static
+mkdir -p ./redis_data
 
-echo "[2/5] login redhat"
+echo "[2/5] change user and group"
+sudo chown -R $(whoami):$(whoami) ./coreVcl ./sqldata ./redis_data 2>/dev/null
+
+echo "[3/5] login redhat"
 podman login registry.redhat.io
 
-echo "[3/5] pull container image..."
+echo "[4/5] pull container image..."
 podman pull registry.redhat.io/rhel9/redis-7:latest
 podman pull registry.redhat.io/rhel9/postgresql-16:latest
 podman pull registry.redhat.io/ubi9/python-39:latest
 podman pull registry.redhat.io/ubi8/nginx-122:latest
 podman pull docker.io/cloudflare/cloudflared:latest
 
-echo "[4/5] initial podman network and container structure ..."
-podman-compose -p vcl up --no-start
+# echo "[4/5] initial podman network and container structure ..."
+# podman-compose -p vcl up --no-start
 
-echo "[4/5] launch all server ..."
-podman-compose -p vcl start
+echo "[5/5] launch all server ..."
+# podman-compose -p vcl start
+podman-compose -p vcl up -d --build
 
 echo "🏁 =========================================="
 echo "🎉 Vision Color Lab 開發環境一鍵啟動成功！"
