18e7ca16941a61f907d3c68909e3f236842cfbb5
geraldwang-spec <65798968+geraldwang-spec@users.noreply.github.com>
2026-07-11 23:24:18 +0800

    adding login page and modify nginx config

---
+++ coreVcl/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+def create_app()->Flask:
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index_html():
+        return render_template('index.html')
+
+    @app.route('/login')
+    def login_html():
+        return render_template('login.html')
+    
+    return app
+
+
+if __name__ == "__main__":
+    flask_app = create_app()
+    flask_app.run(host="0.0.0.0", debug=True, port=5000)
+
