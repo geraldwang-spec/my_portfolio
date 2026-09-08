e12e7740998ae08377802a79e79afbfd39a692f0
qoo <qoo@tt.com>
2026-09-08 18:00:25 +0800

    adding html structure

---
+++ coreVcl/static/js/app/nav-config.js
@@ -0,0 +1,17 @@
+const navConfig = {
+  "live-manager": {
+    label: "Live Manager",
+    sidebarItems: [
+      { id: "dashboard", label: "dashboard" },
+      { id: "profile", label: "Profile" },
+      { id: "settings", label: "Settings" },
+    ]
+  },
+  "video-tools": {
+    label: "video-tools",
+    sidebarItems: [
+      { id: "timeline", label: "timeline" },
+      { id: "export", label: "Export" }
+    ]
+  }
+};
